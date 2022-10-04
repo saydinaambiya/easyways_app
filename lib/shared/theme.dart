828020525332b54bb182b580c475d652b6541926
saydinaambiya <ssyyddn@gmail.com>
2022-10-04 22:57:42 +0700

    add shared variables

---
+++ lib/shared/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//colors
+Color greenColor = const Color(0xFF0EC3AE);
+Color greyColor = const Color(0xFF9698A9);
+Color inactiveColor = const Color(0xFFDBD7EC);
+Color navyColor = const Color(0xFF1E3163);
+Color pinkColor = const Color(0xFFEB70A5);
+Color primaryColor = const Color(0xFF2D46B9);
+Color whiteColor = const Color(0xFFF8F8F8);
+
+//fonts
+TextStyle navyText = GoogleFonts.poppins(
+  color: navyColor,
+);
+TextStyle whiteText = GoogleFonts.poppins(
+  color: whiteColor,
+);
+TextStyle greyText = GoogleFonts.poppins(
+  color: greyColor,
+);
+TextStyle greenText = GoogleFonts.poppins(
+  color: greenColor,
+);
+TextStyle pinkText = GoogleFonts.poppins(
+  color: pinkColor,
+);
+TextStyle blueText = GoogleFonts.poppins(
+  color: primaryColor,
+);
+
+FontWeight extrabold = FontWeight.w800;
+FontWeight semiBold = FontWeight.w600;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight black = FontWeight.w900;
+FontWeight light = FontWeight.w300;
+FontWeight bold = FontWeight.w700;
