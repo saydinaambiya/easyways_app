d7184cea79e1e97294d6275e48682ddd1e7406db
saydinaambiya <ssyyddn@gmail.com>
2022-11-05 23:08:53 +0700

    add destination tile

--- lib/ui/pages/checkout_page.dart
+++ lib/ui/pages/checkout_page.dart
@@ -69,6 +69,85 @@ class CheckoutPage extends StatelessWidget {
       );
     }
 
+    Widget bookingDetails() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(18),
+          color: whiteColor,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 16),
+                  width: 70,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                        "assets/images/image_ciliwung.png",
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Lake Ciliwung",
+                        style: navyText.copyWith(
+                          fontSize: 18,
+                          fontWeight: medium,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        "Tanggerang",
+                        style: greyText.copyWith(
+                          fontWeight: light,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 18,
+                      width: 18,
+                      margin: const EdgeInsets.only(right: 3),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icons/icon_star.png'),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "4.5",
+                      style: navyText.copyWith(
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroundColor,
       body: ListView(
@@ -77,6 +156,7 @@ class CheckoutPage extends StatelessWidget {
         ),
         children: [
           route(),
+          bookingDetails(),
         ],
       ),
     );
