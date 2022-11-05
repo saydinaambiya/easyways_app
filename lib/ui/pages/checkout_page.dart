8e66339be53a60fde52cd553bf0f7904bf04a82a
saydinaambiya <ssyyddn@gmail.com>
2022-11-05 22:56:07 +0700

    create checkout page

---
+++ lib/ui/pages/checkout_page.dart
@@ -0,0 +1,84 @@
+import 'package:easyplane_app/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class CheckoutPage extends StatelessWidget {
+  const CheckoutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget route() {
+      return Container(
+        margin: EdgeInsets.only(top: 50),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              width: 291,
+              height: 55,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icons/icon_flyplane.png',
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'CGK',
+                      style: navyText.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      'Tanggerang',
+                      style: greyText.copyWith(
+                        fontWeight: light,
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'TLC',
+                      style: navyText.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      'Ciliwung',
+                      style: greyText.copyWith(
+                        fontWeight: light,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        children: [
+          route(),
+        ],
+      ),
+    );
+  }
+}
